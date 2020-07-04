b00bcfaa25f9fd80b21e7a49d9f760e388f61f84
tianmaotao <tianmaotao@bilibili.com>
2020-07-04 11:45:50 +0800

    初始化

---
+++ GitTest/GitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by tianmaotao on 2020/7/4.
+//  Copyright © 2020 tianmaotao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
